8afe4aad11a3387d445402408603d779e8586e71
divya <di>
2026-04-03 23:02:43 +0530

    Emergency fix: API URL and Server cleanup

--- client/src/api.js
+++ client/src/api.js
@@ -7,7 +7,6 @@ const API = axios.create({
 API.interceptors.request.use((req) => {
     const user = JSON.parse(localStorage.getItem('user'));
     if (user && user.token) {
-        // Dhyaan dena yahan backticks (`) use kiye hain
         req.headers.Authorization = `Bearer ${user.token}`;
     }
     return req;
